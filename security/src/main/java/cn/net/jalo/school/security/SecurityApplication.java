570ca8e8121eb77be30ceb1ca5aff94e49c592e3
Jalo <65137928@qq.com>
2019-05-07 23:17:04 +0800

    Create project.

---
+++ security/src/main/java/cn/net/jalo/school/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.net.jalo.school.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
